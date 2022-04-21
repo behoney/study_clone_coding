e7a7387672007058addea8f894d20a080f8c39bd
be.honey <be.honey@si-analytics.ai>
2022-04-21 08:22:11 +0000

    [fix] package versioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import IndexPage from './pages/IndexPage';
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={IndexPage} />
+        <Route path='/' component={IndexPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
