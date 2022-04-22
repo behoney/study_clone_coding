4b453ba029aae549e916f11203962042851366fe
be.honey <be.honey@si-analytics.ai>
2022-04-22 04:49:34 +0000

    [add] auth actions

--- redux/modules/create.ts
+++ redux/modules/create.ts
@@ -1,4 +1,6 @@
 import { applyMiddleware, createStore } from "redux"
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducer from "./reduce"
 
 const create = () => {
     const store = createStore(reducer, composeWithDevTools(applyMiddleware()))
