86b783f2b6e4617b7810aad60f75213b33283888
be.honey <be.honey@si-analytics.ai>
2022-04-22 04:29:22 +0000

    [add] routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Home';
+import Edit from './Edit';
+import Detail from './Detail';
+import Add from './Add';
+import Signin from './Signin';
+import NotFound from './NotFound';
+
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/edit/:id" element={<Edit />}></Route>
+        <Route path="/book/:id" element={<Detail />}></Route>
+        <Route path="/add" element={<Add />}></Route>
+        <Route path="/Signin" element={<Signin />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
