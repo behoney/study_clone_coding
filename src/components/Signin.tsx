2537e42f986b212e411de6c664dcece8feb6d60a
be.honey <be.honey@si-analytics.ai>
2022-04-22 05:28:46 +0000

    [add] impl: signin component, type bugs exist

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import styles from "./Signin.module.scss"
+import { Button, Row, Col, Input } from 'antd'
 
 function Signin() {
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    // const emailRef = useRef<Input>(null);
+    // const passwordRef = useRef<Input>(null);
+
+    function click() {
+        const vals = [emailRef.current, passwordRef.current]
+        console.log(vals);
+    }
+
     return (
-        <div>
-            <h1>
-                Signin
-            </h1>
-        </div>
+        <Row>
+            <Col span={24}>
+                <Row>
+                    <Col span={24}></Col>
+                    <Col span={24}>
+                        <div>My books</div>
+                        <div>Please Note Your Opinion</div>
+                        <div></div>
+                        <div>Email
+                            <span className={styles.required}> *</span>
+                        </div>
+                        <div>
+                            <Input ref={emailRef} placeholder="Email" autoComplete="email" name="email"></Input>
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row>
+
+                    <Col span={24}>
+                        <div>Password
+                            <span className={styles.required}> *</span>
+                        </div>
+                        <div>
+                            <Input ref={passwordRef} type="password" placeholder="password" autoComplete="password" name="password"></Input>
+                        </div>
+                    </Col>
+                </Row>
+
+                <Button onClick={() => click()}>Signin</Button>
+            </Col>
+        </Row>
     )
 }
 
